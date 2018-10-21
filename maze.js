7f05f3fe781bc6e0a13af0f18e9ff669f01c581d
Richardo21 <richardostewart@yahoo.com>
2018-10-20 17:10:50 -0700

    Exercise 4

--- maze.js
+++ maze.js
@@ -12,6 +12,7 @@ window.onload = function()
 
 
 	end.onmouseover = congratsMsg;
+
 	for (var j = 0; j < boundaries.length - 1; j++)
 	{
 		boundaries[j].onmouseover = touchMazeWall;
@@ -20,10 +21,11 @@ window.onload = function()
 function touchMazeWall()
 	{
 		if (finishGame)
-			{
+			
 				if (touchWall)
 					{
 						state.innerText = 'You Lose! Click the "S" to start over.';
+
 						for (var x = 0; x < boundaries.length - 1; x++)
 						{
 							boundaries[x].className += "youlose";
@@ -38,5 +40,17 @@ function congratsMsg()
 		{
 			state.innerText = 'You Win! Click the "S" to reset and play again.';
 		}
+		
+	}
+
+function restartGame()
+	{
+		finishGame =  false;
+		touchWall = false;
+		state.innerText = 'Hover the cursor over the "S" to begin.';
+
+		for (var x = 0; var x < boundaries.length - 1; x++)
+		{
+			boundaries[i].className = "boundary";
 		}
 	}
